8ff08f74186f2ea39ca0f59da0e5f0a08556b1ca
anand8533 <161494555+anand8533@users.noreply.github.com>
2024-07-30 10:10:20 -0400

    1
    1

---
+++ src/main/java/com/nt/bootrestproj_restapi_providerapp_crudoperations_errors/BootRestProjRestApiProviderAppCrudOperationsErrorsApplication.java
@@ -0,0 +1,13 @@
+package com.nt.bootrestproj_restapi_providerapp_crudoperations_errors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestProjRestApiProviderAppCrudOperationsErrorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRestProjRestApiProviderAppCrudOperationsErrorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nt/bootrestproj_restapi_providerapp_crudoperations_errors/BootRestProjRestApiProviderAppCrudOperationsErrorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt.bootrestproj_restapi_providerapp_crudoperations_errors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestProjRestApiProviderAppCrudOperationsErrorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
